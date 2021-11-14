7acd2d507f5306611c3f574fcb547b8aa905bb77
Yvette <nchombuayvta@gmil.com>
2021-11-14 16:03:53 +0100

    media queries

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const button = document.querySelector('.navbar-toggler ')
+const clickedItem = document.querySelector('ul')
+
+button.addEventListener('click', () => {
+    clickedItem.classList.toggle('active')
+    button.classList.toggle('active')
+   
+
+
+})
